94e279efb05bd4d069a92516214f827847abc1ff
Fabio Vilela <fbvilela@gmail.com>
2012-05-17 00:26:50 +1000

    asdf

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -11,3 +11,12 @@ class Contact < ActiveResource::Base
   end
 
 end
+
+class ActiveResource::Base
+def self.instantiate_collection(collection, prefix_options = {})
+          puts "instantiate collection from my patch..."
+          collection = collection["contacts"]
+      
+          collection.collect! { |record| instantiate_record(record, prefix_options) }
+ end
+end
