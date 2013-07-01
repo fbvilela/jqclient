f87ac7a7eefeb970f3b6183e65a3c2f556c1591f
Fabio Vilela <fabio@iproperty.com.au>
2013-07-01 12:02:39 +1000

    trying to test the caching on staging.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,8 +1,5 @@
 Jqclient::Application.configure do
   
-  
- 
-  
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
@@ -15,7 +12,7 @@ Jqclient::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
@@ -32,6 +29,8 @@ Jqclient::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  config.cache_store = :dalli_store
 
   # Do not compress assets
   config.assets.compress = false
@@ -40,8 +39,9 @@ Jqclient::Application.configure do
   config.assets.debug = true
 
   # api connection related 
-  config.idashboard_url = "http://megatron.idashboard.com.au"
-  config.app_key = "XrQ0mn5VHlbk5fhPuYUVyavBEqjsgD1of6DlhlM0"
-  config.app_secret = "qKKcy9hPFyBu2KCsgTJOxWt5W7vFoTpCV83L7wPF"
+  config.idashboard_url = "http://ec2-54-252-30-110.ap-southeast-2.compute.amazonaws.com"
+  config.app_key = "lyexzv19zd79eiyXZmdRdj4YIzDTheGR14Ph8oUw"
+  config.app_secret = "RiMwAXwOqTijrltk8uC0N4G5Vz3rmQVoJbijIIcW"
+  
 
 end
