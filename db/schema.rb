9a04fcb02448ad30559ee153e84489f7ba025713
Fabio <fabio@ipropertys-imac.iproperty.com.au>
2014-06-11 16:58:14 +1000

    upgraded from rails 4 to 4.1.1

--- db/schema.rb
+++ db/schema.rb
@@ -9,16 +9,16 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130611022103) do
+ActiveRecord::Schema.define(version: 20130611022103) do
 
-  create_table "login_histories", :force => true do |t|
+  create_table "login_histories", force: true do |t|
     t.integer  "employee_id"
     t.string   "login"
     t.string   "name"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
